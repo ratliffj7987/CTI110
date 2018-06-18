c50a2646a1fed5a9fcda8e70ea6e57aa1e5d5341
ratliffj7987 <40367994+ratliffj7987@users.noreply.github.com>
2018-06-18 14:34:39 -0400

    Add files via upload
    Fist Repository Hype

---
+++ P2T1_SalesPrediction_RatliffJoseph.py
@@ -0,0 +1,18 @@
+
+# CTI-110 
+# P2T1 - Sales Prediction 
+# Joseph Ratliff
+# Date  6/17/2018
+#
+
+
+# Getting the total sales
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23% of total sales.
+
+profit = total_sales * 0.23
+
+# Display the profit
+
+print('The profit is $', format(profit, ',.2f'))
